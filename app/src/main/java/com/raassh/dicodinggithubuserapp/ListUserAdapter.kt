9b073c66bb26d1fd38be3bf83a63ce18d8edf16a
raassh-23 <muh.nur.abdurrauf@gmail.com>
2022-02-28 15:48:55 +0700

    add list users

--- app/src/main/java/com/raassh/dicodinggithubuserapp/ListUserAdapter.kt
+++ app/src/main/java/com/raassh/dicodinggithubuserapp/ListUserAdapter.kt
@@ -1,24 +1,42 @@
 package com.raassh.dicodinggithubuserapp
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.raassh.dicodinggithubuserapp.databinding.ItemUserDetailBinding
 
-class ListUserAdapter : RecyclerView.Adapter<ListUserAdapter.ViewHolder>() {
-    class ViewHolder(val binding: ItemUserDetailBinding) : RecyclerView.ViewHolder(binding.root) {
+class ListUserAdapter(private val listUser: ArrayList<User>) :
+    RecyclerView.Adapter<ListUserAdapter.ViewHolder>() {
+    private lateinit var onItemClickCallback: OnItemClickCallback
 
+    interface OnItemClickCallback {
+        fun onItemClicked(user: User)
     }
 
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    class ViewHolder(val binding: ItemUserDetailBinding) : RecyclerView.ViewHolder(binding.root)
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val binding =
+            ItemUserDetailBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val (username, name, _, _, _, _, _, avatar) = listUser[position]
+
+        holder.binding.photo.setImageResource(avatar)
+        holder.binding.tvItemName.text = name
+        holder.binding.tvItemUsername.text = username
+
+        holder.itemView.setOnClickListener {
+            onItemClickCallback.onItemClicked(listUser[holder.adapterPosition])
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = listUser.size
 
 }
--- app/src/main/java/com/raassh/dicodinggithubuserapp/MainActivity.kt
+++ app/src/main/java/com/raassh/dicodinggithubuserapp/MainActivity.kt
@@ -2,10 +2,65 @@ package com.raassh.dicodinggithubuserapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.raassh.dicodinggithubuserapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val list = ArrayList<User>()
+
+    private val listUsers: ArrayList<User>
+        get() {
+            val names = resources.getStringArray(R.array.name)
+            val usernames = resources.getStringArray(R.array.username)
+            val locations = resources.getStringArray(R.array.location)
+            val repos = resources.getIntArray(R.array.repository)
+            val companies = resources.getStringArray(R.array.company)
+            val followers = resources.getIntArray(R.array.followers)
+            val followings = resources.getIntArray(R.array.following)
+            val photos = resources.obtainTypedArray(R.array.avatar)
+
+            val tempList = ArrayList<User>()
+            for (i in names.indices) {
+                val user = User(
+                    names[i],
+                    usernames[i],
+                    locations[i],
+                    repos[i],
+                    companies[i],
+                    followers[i],
+                    followings[i],
+                    photos.getResourceId(i, -1)
+                )
+                tempList.add(user)
+            }
+
+            return tempList
+        }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvUsers.setHasFixedSize(true)
+
+        list.addAll(listUsers)
+        showRecyclerList()
+    }
+
+    private fun showRecyclerList() {
+        binding.rvUsers.layoutManager = LinearLayoutManager(this)
+
+        val listUserAdapter = ListUserAdapter(list)
+        binding.rvUsers.adapter = listUserAdapter
+
+        listUserAdapter.setOnItemClickCallback(object : ListUserAdapter.OnItemClickCallback {
+            override fun onItemClicked(user: User) {
+                Toast.makeText(this@MainActivity, user.toString(), Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
