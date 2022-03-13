791b8b6d222d8c6f352802eccce6a3a0438d4d55
raassh-23 <muh.nur.abdurrauf@gmail.com>
2022-03-13 22:32:04 +0700

    add retrofit service and config

---
+++ app/src/main/java/com/raassh/dicodinggithubuserapp/ApiService.kt
@@ -0,0 +1,33 @@
+package com.raassh.dicodinggithubuserapp
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("search/users")
+    @Headers("Authorization: token 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")
+    fun getSearchedUsers(
+        @Query("q") query: String
+    ): Call<SearchUserResponse>
+
+    @GET("users/{username}")
+    @Headers("Authorization: token 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")
+    fun getUserDetail(
+        @Path("username") username: String
+    ): Call<UserDetailResponse>
+
+    @GET("users/{username}/followers")
+    @Headers("Authorization: token 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")
+    fun getUserFollowers(
+        @Path("username") username: String
+    ): Call<ListUsersResponse>
+
+    @GET("users/{username}/following")
+    @Headers("Authorization: token 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")
+    fun getUserFollowing(
+        @Path("username") username: String
+    ): Call<ListUsersResponse>
+}
